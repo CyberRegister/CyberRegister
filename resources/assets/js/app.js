045099b3f70526d5a3c2620cc8b33bb82474258e
Anne Jan Brouwer <brouwer@annejan.com>
2018-02-13 23:27:56 +0100

    Ont Cyber JS-ed

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,34 +16,4 @@ $(document).ready(function () {
             $form.trigger('submit')
         })
     })
-
-    // Cyber kleuren
-    $('*').each(function() {
-        //kleur 1
-        if(
-            $(this).css("background-color") === "rgb(249, 184, 30)" ||
-            $(this).css("background-color") === "#f9b81e") {
-            $(this).css("background-color","#76d2b6");
-            //reverse colors
-            //$(this).find('li a').css("color","#fff");
-            //$(this).find('li.active a').css("color","#111");
-        }
-        //kleur 2
-        if(
-            $(this).css("background-color") === "rgb(252, 234, 186)" ||
-            $(this).css("background-color") === "#fceaba") {
-            $(this).css("background-color","#d6f1e9");
-        }
-        if(
-            $(this).css("border-bottom-color") === "rgb(252, 234, 186)" ||
-            $(this).css("border-bottom-color") === "#fceaba") {
-            $(this).css("border-bottom-color","#d6f1e9");
-        }
-        //kleur 3
-        if(
-            $(this).css("background-color") === "rgb(252, 245, 226)" ||
-            $(this).css("background-color") === "#fcf5e2") {
-            $(this).css("background-color","#ebf8f4");
-        }
-    });
 });
