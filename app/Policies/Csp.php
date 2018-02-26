d03b039e5d01d6fcf7d73d8aacec2091cb4f8627
Anne Jan Brouwer <brouwer@annejan.com>
2018-02-26 17:19:35 +0100

    Csp attempt 2

--- app/Policies/Csp.php
+++ app/Policies/Csp.php
@@ -1,30 +1,20 @@
 <?php
 namespace App\Policies;
 
-use Spatie\Csp\Policies\Policy;
 use Spatie\Csp\Directive;
+use Spatie\Csp\Policies\Basic;
 
-class Csp extends Policy
+class Csp extends Basic
 {
     /**
      * @throws \Spatie\Csp\Exceptions\InvalidDirective
      */
     public function configure()
     {
+        parent::configure();
         $this
-            ->addDirective(Directive::BASE, 'self')
-            ->addDirective(Directive::CONNECT, 'self')
-            ->addDirective(Directive::DEFAULT, 'self')
-            ->addDirective(Directive::FORM_ACTION, 'self')
-            ->addDirective(Directive::IMG, 'self')
-            ->addDirective(Directive::MEDIA, 'self')
-            ->addDirective(Directive::OBJECT, 'none')
-            ->addDirective(Directive::SCRIPT, 'self')
-            ->addDirective(Directive::STYLE, 'self')
             ->addDirective(Directive::STYLE, 'https:')
             ->addDirective(Directive::FONT, 'self')
-            ->addDirective(Directive::FONT, 'https:')
-            ->addNonceForDirective(Directive::SCRIPT)
-            ->addNonceForDirective(Directive::STYLE);
+            ->addDirective(Directive::FONT, 'https:');
     }
 }
