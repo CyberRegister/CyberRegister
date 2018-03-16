dffdd5218c7048e29cde21950c7da5ea095b84cf
Anne Jan Brouwer <brouwer@annejan.com>
2018-03-16 23:45:38 +0100

    Teksten

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,25 +1,17 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-    <p>Lorem ipsum dolor amet asymmetrical dreamcatcher seitan, retro chartreuse farm-to-table wolf viral edison
-        bulb yr snackwave gochujang pickled umami. Hot chicken bespoke food truck, cold-pressed listicle
-        single-origin coffee typewriter sartorial viral blog. Hoodie fanny pack put a bird on it forage.<br>
-        Shoreditch letterpress you probably haven't heard of them keytar. Sartorial narwhal thundercats
-        meggings tote bag trust fund put a bird on it pabst. Activated charcoal art party lumbersexual, before
-        they sold out put a bird on it fashion axe pug +1 narwhal iceland.
-    </p><p>
-        Truffaut subway tile godard gastropub, green juice cloud bread authentic tumblr. Roof party disrupt
-        single-origin coffee enamel pin narwhal tofu yuccie fixie pabst you probably haven't heard of them
-        la croix. Mlkshk 8-bit unicorn YOLO, occupy neutra narwhal enamel pin pok pok letterpress affogato.<br>
-        Man braid flexitarian fixie edison bulb chartreuse vaporware green juice, snackwave readymade forage
-        chicharrones. Craft beer keytar selvage, man braid pug fixie plaid banh mi meditation gluten-free
-        iceland hot chicken next level edison bulb vaporware. Locavore shabby chic everyday carry, williamsburg
-        shaman artisan keffiyeh cliche.
-    </p><p>
-        Adaptogen pabst gentrify hoodie. Typewriter photo booth copper mug viral crucifix synth.<br>
-        Ennui marfa cliche DIY, messenger bag mumblecore snackwave banjo chia cronut slow-carb tumblr
-        shabby chic. Swag portland crucifix kombucha edison bulb. Cray shaman direct trade chartreuse,
-        cold-pressed hammock tousled.
+    <h2>Cyberregister</h2>
+    <p>
+        Het register bestaat uit experts op verscheidene gebieden van cyber die allen beschikken over en hoge mate van cyberbewustzijn.<br>
+        Middels het cyberregister zijn cybercompetenties inzichtelijk, om te helpen bij het zoeken naar de juiste experts om cyberweerbaarheids processen te begeleiden.
+    </p>
+    <p>
+        Een cyberexpert dient per twee jaar voldoende Permanente Cyber Expertise (PCE) punten te behalen, hoeveel punten daar voor nodig zijn hangt af van de geaccrediteerde cyberexpertises.<br>
+        Alleen experts die voldoen aan de kwaliteitseisen die het Cyberregister stelt, staan in het Register ingeschreven.
+    </p>
+    <p>
+        Er zijn de afgelopen maand reeds {{ \App\User::where('created_at', '<', Carbon\Carbon::now()->subMonth())->count() }} nieuwe experts ingeschreven.
     </p>
     <p>Bekijk de code op <a href="https://github.com/CyberRegister/CyberRegister">GitHub</a>.</p>
 </div>
