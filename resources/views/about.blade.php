76a4913cd93fc3a2db725e4b751b61e35e97aac4
Anne Jan Brouwer <brouwer@annejan.com>
2018-03-16 11:27:17 +0100

    Tekst

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,12 +1,29 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
+    <h2>Wat is het Cyberregister</h2>
     <p>
-        Lorem ipsum dolor amet organic readymade chillwave try-hard, trust fund iPhone ramps chia shaman
-        meditation cloud bread affogato art party echo park lyft. Hammock mlkshk heirloom, affogato plaid
-        single-origin coffee fingerstache roof party mumblecore. Heirloom echo park beard vice hella paleo
-        enamel pin marfa. Ugh selfies before they sold out biodiesel air plant, tbh cronut lyft stumptown
-        fixie scenester next level.
+        Cyberregister is hét onafhankelijke NATIONALE REGISTER VAN NEDERLAND waarin alle cyberexperts geregistreerd kunnen worden.<br>
+        Het cyberregister is een openbaar toegankelijke database waarin uitsluitend cybergecertificeerde experts opgenomen worden.<br>
+        Dit zijn experts gecertificeerd op basis van de cyberkwaliteitsnormen, zoals CEX, CCX, HFA, CPX, CCM, HCE, CDA, CDB, CDC, CDD, CDE, CDF, CDO, CCW, ILW, HMA, HMB, SEA, SEB, VHF en COR.<br>
+        Een vermelding in het Cyberregister levert een toegevoegde waarde aan cyberexperts door grotere en gemakkelijke vindbaarheid, waardoor o.a. de kansen op nieuwe cybercontacten en cyberklanten kan toenemen.
+    </p>
+    <h2>Waarom registreren?</h2>
+    <p>
+        Om de veiligheid in de cyber te vergroten, is Stichting Eurocyber in 2018 gestart met het continu screenen van cyberexperts.<br>
+        We kijken dagelijks of ze feiten hebben gepleegd of uitlatingen hebben gedaan die niet verenigbaar zijn met werken in de cyber.<br>
+        Met het Cyberregister breiden we deze screening uit naar iedereen die in de cyber werkt en een bewijs van cyberexpertise nodig heeft.
+    </p>
+    <h2>Voor bedrijven</h2>
+    <p>
+        Er is een sterk groeiende behoefte aan gekwalificeerd personeel op het gebied van cybersecurity binnen de industriële en installatiesectoren.<br>
+        Uitval van primaire bedrijfsprocessen kost geld en kan gevaarlijke situaties opleveren.<br>
+        Het Nederlandse bedrijfsleven en de overheid zijn zich vaak niet bewust van de omvang, duur, impact en gevolgen van cyberaanvallen.<br>
+        Organisaties zien de kosten voor het herstellen van de gevolgen als een acceptabel bedrijfsrisico.<br>
+        Men gaat ervan uit dat dit goedkoper is dan investeren in structurele preventieve maatregelen tegen cyberaanvallen.<br>
+        Hierbij gaan deze organisaties voorbij aan de mogelijke (financiële) gevolgen op langere termijn: imagoschade, verlies van marktaandeel en aantasting van de concurrentiepositie.<br>
+        Het aantal cyberaanvallen dat de Nederlandse hacker inlichtingendiensten zien, en de omvang van de (economische) schade, bewijzen dat spionageaanvallen een reëel risico vormen voor het Nederlandse bedrijfsleven en de overheid.<br>
+        Het cyberregister helpt bedrijven bij het filteren van cybercharlatans en cyberkwakzalvers, zo kunnen bedrijven er zich van verzekeren dat ze met serieuze cyberexperts van doen hebben.
     </p>
     <p>
         Mede mogelijk gemaakt door:
